6e0a1c2cee5651886d056a6515c3019ed507f875
kwb020312 <kwb020312@naver.com>
2021-08-01 17:48:28 +0900

    #1.4 WebSocket Message Sending

--- src/server.js
+++ src/server.js
@@ -15,10 +15,13 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-  console.log(socket);
-}
-
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+  socket.send("hello!!!");
+  console.log("Connected to Server!");
+  socket.on("message", (message) => {
+    console.log(message);
+  });
+  socket.on("close", () => console.log("Disconnected From Browser"));
+});
 
 server.listen(3000, handleListen);
