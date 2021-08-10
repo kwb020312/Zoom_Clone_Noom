95d2887355947b3a716b0f294ad4d169b2f577ba
kwb020312 <kwb020312@naver.com>
2021-08-10 23:45:41 +0900

    #3.8 Senders

--- src/server.js
+++ src/server.js
@@ -24,10 +24,9 @@ wsServer.on("connection", (socket) => {
     socket.join(roomName);
     socket.to("roomName").emit("welcome");
   });
-  socket,
-    on("offer", (offer, roomName) => {
-      socket.to(roomName).emit("offer", offer);
-    });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
   socket.on("answer", (answer, roomName) => {
     socket.to(roomName).emit("answer", answer);
   });
